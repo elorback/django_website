c3fa963990c5a7ffebce78000e10d977ed16e6fb
elorback <elorback@gmail.com>
2023-09-07 19:28:04 -0700

    updated formatting of html files

--- django_app/urls.py
+++ django_app/urls.py
@@ -6,12 +6,12 @@ urlpatterns = [
     path('registration/',views.registerUser, name='registration'),
     path('login/',views.login_method,name='login_method'),
     path('logout/',views.user_logout,name='user_logout'),
-    path('add_item/', views.add_item, name='add_item'),
+    path('user_page/add_item/', views.add_item, name='add_item'),
     path('user_page/', views.user_page, name='user_page'),
-    path('user_items',views.user_items,name='user_items'),
-    path('multiple_posts',views.multiple_posts,name='multiple_posts'),
-    path('delete_profile',views.delete_profile,name='delete_profile'),
-    path('user_settings',views.user_settings,name='user_settings'),
+    path('user_page/user_items/',views.user_items,name='user_items'),
+    path('user_page/multiple_posts/',views.multiple_posts,name='multiple_posts'),
+    path('user_settings/delete_profile/',views.delete_profile,name='delete_profile'),
+    path('user_settings/',views.user_settings,name='user_settings'),
 
   
 ]
