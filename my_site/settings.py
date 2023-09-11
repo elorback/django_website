9d2e60792ebd242d02bda0ced8254fc4dc60deed
elorback <elorback@gmail.com>
2023-09-11 16:29:26 -0700

    updated password for general purposes for database

--- my_site/settings.py
+++ my_site/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_db',
         'USER':'root',
-        'PASSWORD':'THESEbiscuts78!',
+        'PASSWORD':'password1234',
         'HOST':'localhost',
         'PORT':'3306'
     }
